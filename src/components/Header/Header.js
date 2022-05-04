00065edbf1cd94f9e8be757fcaf642c6ae9c231f
vinitpulstya <vinitsharma9876@gmail.com>
2022-05-04 18:04:46 +0530

    add ons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,39 +5,61 @@ import user from '../../img/user.jpg';
 // import iconMagnifyingGlass from '../../img/SVG/magnifying-glass.svg';
 
 function Header() {
-    return (
-      <header className='header'>
-        <img src = {logo} alt='trillo logo' className='logo' />
-        <form action='#' className='search'>
-          <input type='text' className='search__input' placeholder='Search hotels' />
-          <button className='search__button'>
-            <svg className='search__icon'>
-              <use xlinkHref={`${sprite}#icon-magnifying-glass`}></use>
-            </svg>
-          </button>
-        </form>
+  return (
+    <header className='header'>
+      <img src={logo} alt='trillo logo' className='logo' />
+      <form action='#' className='search'>
+        <input type='text' className='search__input' placeholder='Search hotels' />
+        <button className='search__button'>
+          <svg className='search__icon'>
+            <use xlinkHref={`${sprite}#icon-magnifying-glass`}></use>
+          </svg>
+        </button>
+      </form>
 
-        <nav className='user-nav'>
-          <div className='user-nav__icon-box'>
-            <svg className='user-nav__icon'>
-              <use xlinkHref={`${sprite}#icon-bookmark`}></use>
-            </svg>
-            <span className='user-nav__notification'>7</span>
-          </div>
-          
-          <div className='user-nav__icon-box'>
-            <svg className='user-nav__icon'>
-              <use xlinkHref={`${sprite}#icon-chat`}></use>
-            </svg>
-            <span className='user-nav__notification'>13</span>
-          </div>
-          <div className='user-nav__user'>
-            <img src={user} alt='user' className='user-nav__user-photo'/>
-            <span className='user-nav__user-name'>Jonas</span>
-          </div>
-        </nav>
-      </header>
-    );
-  }
-  
-  export default Header;
+      <nav className='user-nav'>
+
+        <div className='user-nav__icon-box'>
+          <svg className='user-nav__icon'>
+            <use xlinkHref={`${sprite}#icon-bookmark`}></use>
+          </svg>
+          <span className='user-nav__notification'>7</span>
+        </div>
+
+        <div className='nav-content'>
+            <ul className='nav-content__list'>
+              <li className='nav-content__list-item'>Something</li>
+              <li className='nav-content__list-item'>Something else</li>
+            </ul>
+        </div>
+
+        <div className='user-nav__icon-box'>
+          <svg className='user-nav__icon'>
+            <use xlinkHref={`${sprite}#icon-chat`}></use>
+          </svg>
+          <span className='user-nav__notification'>13</span>
+        </div>
+
+        <div className='nav-content'>
+            <ul className='nav-content__list'>
+              <li className='nav-content__list-item'>Something</li>
+              <li className='nav-content__list-item'>Something else</li>
+              <li className='nav-content__list-item'>Something else</li>
+              <li className='nav-content__list-item'>Something else</li>
+              <li className='nav-content__list-item'>Something else</li>
+              <li className='nav-content__list-item'>Something else</li>
+    
+            </ul>
+        </div>
+
+        <div className='user-nav__user'>
+          <img src={user} alt='user' className='user-nav__user-photo' />
+          <span className='user-nav__user-name'>Jonas</span>
+        </div>
+
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,11 +2,13 @@ import './sidebar.scss';
 import sprite from '../../img/sprite.svg';
 
 function Sidebar() {
+    // let activeClassName = 'side-nav__item--active';
+
     return (
         <nav className='sidebar'>
             <ul className='side-nav'>
                 <li className='side-nav__item side-nav__item--active'>
-                    <a href='#' className='side-nav__link'>
+                    <a href={'/hotel'} className='side-nav__link'>
                         <svg className='side-nav__icon'>
                             <use xlinkHref={`${sprite}#icon-home`}></use>
                         </svg>
@@ -14,7 +16,7 @@ function Sidebar() {
                     </a>
                 </li>
                 <li className='side-nav__item'>
-                    <a href='#' className='side-nav__link'>
+                    <a href='/flight' className='side-nav__link'>
                         <svg className='side-nav__icon'>
                             <use xlinkHref={`${sprite}#icon-aircraft-take-off`}></use>
                         </svg>
@@ -22,7 +24,7 @@ function Sidebar() {
                     </a>
                 </li>
                 <li className='side-nav__item'>
-                    <a href='#' className='side-nav__link'>
+                    <a href='carrental' className='side-nav__link'>
                         <svg className='side-nav__icon'>
                             <use xlinkHref={`${sprite}#icon-key`}></use>
                         </svg>
@@ -30,7 +32,7 @@ function Sidebar() {
                     </a>
                 </li>
                 <li className='side-nav__item'>
-                    <a href='#' className='side-nav__link'>
+                    <a href='/tours' className='side-nav__link'>
                         <svg className='side-nav__icon'>
                             <use xlinkHref={`${sprite}#icon-map`}></use>
                         </svg>
